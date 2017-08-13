39203fcdac6ce4e83c41b91e626899a26c3688c7
lindl <lin_donglian@163.com>
2017-08-13 18:25:52 +0800

    页面组件

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-row>
+    <el-col :span="24">
+      <footer>
+        author:LinDL
+      </footer>
+    </el-col>
+  </el-row>
+</template>
+<style>
+  footer{
+    height: 3rem;
+    background-color: #1F2D3D;
+    color: darkgray;
+    text-align: center;
+  }
+</style>
