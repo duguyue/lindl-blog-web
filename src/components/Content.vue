ad3368ba189b154a2d58533617d848395ff5000a
lindl <lin_donglian@163.com>
2017-08-24 18:02:05 +0800

    通过axios异步请求获取表格数据

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -23,7 +23,7 @@
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
-      :current-page="currentPage4"
+      :current-page="currentPage1"
       :page-sizes="[100, 200, 300, 400]"
       :page-size="100"
       layout="total, sizes, prev, pager, next, jumper"
@@ -37,31 +37,6 @@
   import axios from 'axios';
   Vue.prototype.$ajax = axios
 
-//  var t1=[{
-//  date: '2016-05-02',
-//    name: '王小虎',
-//    address: '上海市普陀区金沙江路 1518 弄'
-//  }, {
-//    date: '2016-05-04',
-//      name: '王小虎',
-//      address: '上海市普陀区金沙江路 1517 弄'
-//  }, {
-//    date: '2016-05-01',
-//      name: '王小虎',
-//      address: '上海市普陀区金沙江路 1519 弄'
-//  }, {
-//    date: '2016-05-03',
-//      name: '王小虎',
-//      address: '上海市普陀区金沙江路 1516 弄'
-//  }];
-
-
-//  let vm = this;
-//  vm.loading = true;
-
-  /*
-
-*/
   export default {
     methods: {
       handleSizeChange(val) {
@@ -75,28 +50,19 @@
       return {
         tableData: [],
         currentPage1: 5,
-        currentPage2: 5,
-        currentPage3: 5,
-        currentPage4: 4
       }
     },
     created(){
       axios({
         method: 'get',
         url: 'static/api/t1.json',
-//        responseType: 'json'
       })
         .then(function (response) {
-            console.log(response);
-//      vm.loading = false;
-          // response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
           var str=response.data;
           var arr=eval(str);
           this.tableData=arr;
 
-          console.log('----返回数据-----');
-          console.log(arr);
-          ////在这里需要bind(this)，才能访问到Vue实例中的data
+          //在这里需要bind(this)，才能访问到Vue实例中的data
         }.bind(this));
     }
   };
