7b6be928b6d5a82168190239cf4674fbce8bdf40
lindl <lin_donglian@163.com>
2017-08-24 17:57:07 +0800

    通过axios异步请求获取表格数据

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -34,31 +34,33 @@
 </template>
 <script>
   import Vue from 'Vue'
-//  import axios from 'axios';
-//  Vue.prototype.$ajax = axios
+  import axios from 'axios';
+  Vue.prototype.$ajax = axios
 
-//  var t1=;
+//  var t1=[{
+//  date: '2016-05-02',
+//    name: '王小虎',
+//    address: '上海市普陀区金沙江路 1518 弄'
+//  }, {
+//    date: '2016-05-04',
+//      name: '王小虎',
+//      address: '上海市普陀区金沙江路 1517 弄'
+//  }, {
+//    date: '2016-05-01',
+//      name: '王小虎',
+//      address: '上海市普陀区金沙江路 1519 弄'
+//  }, {
+//    date: '2016-05-03',
+//      name: '王小虎',
+//      address: '上海市普陀区金沙江路 1516 弄'
+//  }];
 
 
 //  let vm = this;
 //  vm.loading = true;
 
   /*
-  axios({
-    method: 'get',
-    url: 'static/api/t1.json',
-    responseType: 'json'
-  })
-    .then(function (response) {
-//      vm.loading = false;
-     // response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-      t1=response.data;
 
-      this.tableData=t1;
-
-      console.log('----返回数据-----');
-      console.log(t1);
-    });
 */
   export default {
     methods: {
@@ -71,28 +73,31 @@
     },
     data() {
       return {
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }],
+        tableData: [],
         currentPage1: 5,
         currentPage2: 5,
         currentPage3: 5,
         currentPage4: 4
       }
+    },
+    created(){
+      axios({
+        method: 'get',
+        url: 'static/api/t1.json',
+//        responseType: 'json'
+      })
+        .then(function (response) {
+            console.log(response);
+//      vm.loading = false;
+          // response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+          var str=response.data;
+          var arr=eval(str);
+          this.tableData=arr;
+
+          console.log('----返回数据-----');
+          console.log(arr);
+          ////在这里需要bind(this)，才能访问到Vue实例中的data
+        }.bind(this));
     }
   };
 </script>
